4b86c2010364a5eb4a106f0c9a4ccd576a610463
JohnZBurnett <tuninator@gmail.com>
2018-07-23 11:57:58 -0400

    finish

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,1 +1,29 @@
 // Code SearchableMovieReviewsContainer Here
+import React, {Component }from 'react';
+
+class SearchableMovieReviewsContainer extends Component {
+    constructor() {
+        super();
+        
+        this.state = {
+            reviews: [],
+            searchTerm: ""
+        }
+    }
+
+    performSearch = (event) => {
+        this.setState({
+            searchTerm: event.target.value
+        })
+    }
+    render() {
+        return(
+            <div className="searchable-movie-reviews">
+              <label>Enter a search term: </label>
+              <input type="text" onChange={this.performSearch}/>
+            </div>
+        )
+    }
+}
+
+export default SearchableMovieReviewsContainer; 
