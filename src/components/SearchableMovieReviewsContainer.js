ece35b49bc8a7bb1f76fc5add081ae410f8e1b86
Alberto Carreras <a.carreras.c@gmail.com>
2018-07-24 09:49:55 -0400

    hide api key

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -6,11 +6,11 @@ import MovieReviews from './ReviewList';
 import SearchBar from './SearchBar';
 
 
-class SearchableMovieReviewsContainer extends Component {
 
+class SearchableMovieReviewsContainer extends Component {
     constructor(props){
         super(props)
-        this.key = 'c4c50c9ebc8f4db5a0c071c9de2c6d68'
+        this.key = process.env.REACT_APP_API_KEY
         
         this.state = {
            reviews: [],
@@ -43,6 +43,7 @@ class SearchableMovieReviewsContainer extends Component {
     }
 
     render() {
+        
         return (
             <Fragment>
                 <SearchBar 
